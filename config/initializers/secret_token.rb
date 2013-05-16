4e6b55dd426322ce5ed47e0d156e6cb5b97db3ca
saasbook <saasbook@saasbook.(none)>
2013-05-16 11:34:14 -0700

    inicio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cavalga::Application.config.secret_token = '05f290630400f9c4ec0d415b8a5f27f54ff03397bd03ad605f43be81d7d35ed95c49ca11aea4e2248b0c043fc326670d4d6963d0275e3d0bf11d8bea831e7cea'
